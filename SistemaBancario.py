6fe0103d21480050ccb635df12870975b6d41b3f
HeitorSKimura <heitorskimura2010@hotmail.com>
2024-04-12 18:40:16 -0300

    Sistema Pronto

--- SistemaBancario.py
+++ SistemaBancario.py
@@ -36,14 +36,34 @@ while True:
     elif(opcao == "s"):
         print(" Sacar ".center(26,"-"))
         while(True):
-            if(numero_saques <= LIMITE_SAQUES):
-                valor = float(input("Informe o valor que deseja Sacar: "))
-                if()
+            if(numero_saques != LIMITE_SAQUES):
+                if(saldo != 0):
+                    valor = float(input("Informe o valor que deseja Sacar: "))
+                    if(valor <= 500):            
+                        if(valor <= saldo):
+                            extrato += f"Saque: R$ {truncateNum(valor,2)}\n"
+                            numero_saques += 1
+                            saldo -= valor
+                            break
+                        else:
+                            print("Valor excede o valor total da conta!, tente novamente.\n")
+                    else:
+                        print("Valor maximo por saque é de R$ 500.00, tente novamente.")
+                else:
+                    print("Não será possiver sacar o dinheiro por falta de saldo.")
+                    break
+            else:
+                print("Saques diário excedido, tente novamente amanhã.")
+                break
     elif(opcao == "e"):
         print(" Extrato ".center(26,"#"))
-        print(extrato)
+        if(extrato == ""):
+            print("Não foram realizadas movimentações")
+        print(extrato)        
+        print(f"Saldo Atual: {saldo}")
+        print("".center(26,"-"))
     elif(opcao == "q"):
-        print("Sistema Encerrado")
+        print("Sistema Encerrado".center(26,"*"))
         break
     else:
         print("Opção Inválida, por favor insira novamente a operação desejada.")
