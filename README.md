a86956d88833318c7c8861e8671a1b99536e34c1
HeitorSKimura <heitorskimura2010@hotmail.com>
2024-04-29 17:28:54 -0300

    Criando Usuario

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-## 🐱‍👤 Desafio [DIO](https://web.dio.me):
+# 👨🏻‍💻 Desafio [DIO](https://web.dio.me):
+
+## Parte 1
 Fomos contratados por um grande banco para desenvolver o seu novo sistema. Esse banco deseja modernizar suas operações e para isso escolheu a linguagem Python. Para a primeira versão do sistema devemos implementar apenas 3 operações: depósito, saque e extrato.
 
 1. **💰 Operação de Depósito:**
@@ -9,3 +11,54 @@ Fomos contratados por um grande banco para desenvolver o seu novo sistema. Esse 
 
 3. **😱 Operação de Extrato:**
     - Essa operação deve listar todos os depósitos e saques realizados na conta. No fim da listagem deve ser exibido o saldo atual da conta. Se o extrato estiver em branco, exibir a mensagem: Não foram realizadas movimentações. Os valores devem ser exibidos utilizando o formato R$ xxx.xx, exemplo: 1500.45 = R$ 1500.45
+
+## Parte 2
+
+### Objetivo Geral:
+Separar as funções existentes de saque, depósito e extrato em funções. Criar duas novas funções: Cadastrar Usuário (Cliente) e Cadastrar Conta Bancária.
+
+1. **Saque:**
+    - A função saque deve receber os argumentos apenas por nome (keyword only). 
+        - Sugestão dos argumentos
+            - saldo
+            - valor
+            - extrato
+            - limite
+            - numeros_saques
+            - limite_saque
+        - Sugestão de retorno:
+            - Saldo
+            - Extrato
+2. **Depósito:**
+    - A função depósito deve receber os argumentos apenas por posição (positional only).
+        - Sugestão de argumentos:
+            - Saldo
+            - Valor
+            - Extrato
+        - Sugestão de retorno:
+            - Saldo
+            - Extrato
+3. **Extrato:**
+    - A função extrato deve receber os argumentos por posição e nome (positional only and keyword only).
+        - Argumentos posicionais: **saldo**.
+        - Argumentos nomeados: **extrato**
+4. **Novas Funções:**
+    - Criar Usuário
+        - O programa deve armazenar os usuários em uma lista, um usuário e composto por:
+            - nome
+            - data de nascimento
+            - cpf e endereço
+        - O Endereço é uma string com o formato:
+            - logradouro
+            - numero
+            - bairro
+            - cidade/sigla estado
+        - Deve ser armazenado somente os numeros do CPF. Não podemos cadastrar 2 usuários com o mesmo CPF.
+    - Criar Conta Corrente
+        - O programa deve armazenar Contas em uma lista, uma conta é composta por: 
+            - agência
+            - numero da conta
+            - usuário 
+        - O Numero da conta é sequencial, iniciando em 1.
+        - O numero da agência é fixo: "0001".
+        - O Usuário pode ter mais de uma conta, mas uma conta pertence a somente um usuário.
--- SistemaBancario.py
+++ SistemaBancario.py
@@ -3,8 +3,39 @@ import math
 def truncateNum(number, dec_plc):
     return math.floor(number * 10 ** dec_plc) / 10 ** dec_plc
 
+def saque():
+    return
+
 def depositar():
     return
+
+def extrato():
+    return
+
+def criarUsuario(listaUsuario):
+    nome = input("Informe seu nome: ")
+    dataNascimento = input("Informe sua data de nascimento(dia/mes/ano): ")
+    cpf = input("Informe seu CPF: ")
+    endereco = criarEndereco()
+    for i in listaUsuario:
+        if(i == cpf):
+            return "Conta Existente! Tente novamente."
+        else:
+            listaUsuario.append(nome, dataNascimento, cpf, endereco)
+    
+
+def criarConta():
+    return
+
+def criarEndereco():
+    print("Informe seu Endereco(logradouro - numero - bairro - cidade - estado):")
+    logradouro = input("Logradouro: ")
+    numero = input("Numero: ")
+    bairro = input("Bairro: ")
+    cidade = input("Cidade: ")
+    estado = input("Estado(sigla): ")
+    endereco = {"endereco" : {"logradouro":logradouro, "numero":numero, "bairro":bairro, "cidade":cidade, "estado":estado} }
+    return endereco
 
 menu = """
 
@@ -20,53 +51,58 @@ limite = 500
 extrato = ""
 numero_saques = 0
 LIMITE_SAQUES = 3
+lista_Usuario = []
+lista_Conta = []
 
-while True:
+criarUsuario(lista_Usuario)
 
-    opcao = input(menu.lower())
+print(lista_Usuario[0])
+# while True:
 
-    if(opcao == "d"):
-        print(" Depositar ".center(26,"-"))
-        while(True):
-            valor = float(input("Informe o valor que deseja Depositar: "))
-            if(valor < 0):
-                print("\nValor não aceitavel, tente novamente.\n")
-                continue
-            else:
-                saldo += valor
-                extrato += f"Deposito: R$ {truncateNum(valor,2)}\n"
-                break
-    elif(opcao == "s"):
-        print(" Sacar ".center(26,"-"))
-        while(True):
-            if(numero_saques != LIMITE_SAQUES):
-                if(saldo != 0):
-                    valor = float(input("Informe o valor que deseja Sacar: "))
-                    if(valor <= 500):            
-                        if(valor <= saldo):
-                            extrato += f"Saque: R$ {truncateNum(valor,2)}\n"
-                            numero_saques += 1
-                            saldo -= valor
-                            break
-                        else:
-                            print("Valor excede o valor total da conta!, tente novamente.\n")
-                    else:
-                        print("Valor maximo por saque é de R$ 500.00, tente novamente.")
-                else:
-                    print("Não será possiver sacar o dinheiro por falta de saldo.")
-                    break
-            else:
-                print("Saques diário excedido, tente novamente amanhã.")
-                break
-    elif(opcao == "e"):
-        print(" Extrato ".center(26,"#"))
-        if(extrato == ""):
-            print("Não foram realizadas movimentações")
-        print(extrato)        
-        print(f"Saldo Atual: {saldo}")
-        print("".center(26,"-"))
-    elif(opcao == "q"):
-        print("Sistema Encerrado".center(26,"*"))
-        break
-    else:
-        print("Opção Inválida, por favor insira novamente a operação desejada.")
+#     opcao = input(menu.lower())
+
+#     if(opcao == "d"):
+#         print(" Depositar ".center(26,"-"))
+#         while(True):
+#             valor = float(input("Informe o valor que deseja Depositar: "))
+#             if(valor < 0):
+#                 print("\nValor não aceitavel, tente novamente.\n")
+#                 continue
+#             else:
+#                 saldo += valor
+#                 extrato += f"Deposito: R$ {truncateNum(valor,2)}\n"
+#                 break
+#     elif(opcao == "s"):
+#         print(" Sacar ".center(26,"-"))
+#         while(True):
+#             if(numero_saques != LIMITE_SAQUES):
+#                 if(saldo != 0):
+#                     valor = float(input("Informe o valor que deseja Sacar: "))
+#                     if(valor <= 500):            
+#                         if(valor <= saldo):
+#                             extrato += f"Saque: R$ {truncateNum(valor,2)}\n"
+#                             numero_saques += 1
+#                             saldo -= valor
+#                             break
+#                         else:
+#                             print("Valor excede o valor total da conta!, tente novamente.\n")
+#                     else:
+#                         print("Valor maximo por saque é de R$ 500.00, tente novamente.")
+#                 else:
+#                     print("Não será possiver sacar o dinheiro por falta de saldo.")
+#                     break
+#             else:
+#                 print("Saques diário excedido, tente novamente amanhã.")
+#                 break
+#     elif(opcao == "e"):
+#         print(" Extrato ".center(26,"#"))
+#         if(extrato == ""):
+#             print("Não foram realizadas movimentações")
+#         print(extrato)        
+#         print(f"Saldo Atual: {saldo}")
+#         print("".center(26,"-"))
+#     elif(opcao == "q"):
+#         print("Sistema Encerrado".center(26,"*"))
+#         break
+#     else:
+#         print("Opção Inválida, por favor insira novamente a operação desejada.")
